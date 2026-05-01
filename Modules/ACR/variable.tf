b0e057dfde8bbb034ec3efc15b69e74c02f64f97
Your Name <you@example.com>
2026-05-02 00:52:48 +0530

    change version

---
+++ Modules/ACR/variable.tf
@@ -0,0 +1,9 @@
+variable "acr" {
+     type = map(object({
+       name = string
+       location = string
+       resource_group_name = string
+       sku = string
+       admin_enabled = bool
+     })) 
+}
