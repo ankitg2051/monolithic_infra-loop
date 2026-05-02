39a7ee41de123e759dabf1b19ce997a242a04dfe
Your Name <you@example.com>
2026-05-02 22:35:08 +0530

    added webapp

--- Modules/ACR/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "acr" {
-     type = map(object({
-       name = string
-       location = string
-       resource_group_name = string
-       sku = string
-       admin_enabled = bool
-     })) 
-}
---
+++ Modules/app_services/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_service_plan" "pla" {
+  for_each = var.aps
+
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  for_each = var.aps
+
+  name                = each.value.name1
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  service_plan_id     = azurerm_service_plan.pla[each.key].id
+
+  site_config {
+    application_stack {
+      dotnet_version = each.value.dotnet_version
+    }
+  }
+}
